ffa3ec30249d60bd7af6471a2c6f2dde885a6a82
NguyenCT <dangnguyen120699@gmail.com>
2024-07-19 13:49:05 +0700

    upload code

---
+++ test.js
@@ -0,0 +1,3 @@
+const myLog = (name) => {
+  console.log("this is my test function: ", name ?? "Sample name");
+};
---
+++ test_3.js
@@ -0,0 +1,11 @@
+(function () {
+  window.myLog = function (message) {
+    console.log("My Log:", message);
+  };
+
+  window.anotherFunction = function () {
+    console.log("Another function called");
+  };
+
+  console.log("test_2.js script loaded");
+})();
